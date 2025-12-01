4eadedaade3ae5f6243df9936f94b93d97075f94
MYUNGSU\jaung <jaung3195@gmail.com>
2025-12-01 16:53:01 +0900

    add holiday req/resp dto, country req,resp dto, nager dto

--- holidayCountry/src/main/java/com/planitsquare/holidayCountry/country/dto/CountryReqDto.java
+++ holidayCountry/src/main/java/com/planitsquare/holidayCountry/country/dto/CountryReqDto.java
@@ -1,4 +1,13 @@
 package com.planitsquare.holidayCountry.country.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountryReqDto {
+    private String countryCode;
+    private String countryName;
 }
