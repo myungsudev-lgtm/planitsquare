d7568d980a2b2e427e864aab76035cdff5b1beed
MYUNGSU\jaung <jaung3195@gmail.com>
2025-12-03 16:53:33 +0900

    create holiday init set , api controller, service, repository, dto

--- holidayCountry/src/main/java/com/planitsquare/holidayCountry/holiday/dto/HolidayRespDto.java
+++ holidayCountry/src/main/java/com/planitsquare/holidayCountry/holiday/dto/HolidayRespDto.java
@@ -1,4 +1,41 @@
 package com.planitsquare.holidayCountry.holiday.dto;
 
+import com.planitsquare.holidayCountry.country.entity.Country;
+import com.planitsquare.holidayCountry.holiday.entity.Holiday;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HolidayRespDto {
+
+    private LocalDate date;
+    private String localName;
+    private String name;
+    private String country;
+    private Boolean fixed;
+    private Boolean global;
+    private String counties;
+    private Integer launchYear;
+    private List<String> types = new ArrayList<>();
+
+    public static HolidayRespDto from(Holiday h){
+        HolidayRespDto r = new HolidayRespDto();
+        r.date = h.getDate();
+        r.localName = h.getLocalName();
+        r.name = h.getName();
+        r.country = h.getCountry().getCountryCode();
+        r.fixed = h.getFixed() != null ? h.getFixed() : false;
+        r.global = h.getGlobal() != null ? h.getGlobal() : false;
+        r.counties = h.getCounties();
+        r.launchYear = h.getLaunchYear();
+        r.types = h.getTypes() != null ? h.getTypes() : new ArrayList<>();
+        return r;
+    }
 }
--- holidayCountry/src/main/java/com/planitsquare/holidayCountry/holiday/repository/HolidayRepositoryCustom.java
+++ holidayCountry/src/main/java/com/planitsquare/holidayCountry/holiday/repository/HolidayRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.planitsquare.holidayCountry.holiday.repository;
 
+import com.planitsquare.holidayCountry.holiday.entity.Holiday;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
 public interface HolidayRepositoryCustom {
+    Page<Holiday> search(Integer year, String country, String type, LocalDate fromDate, LocalDate toDate, Pageable pageable);
+
 }
